fc574e4fac4cb40a611d81f8c54d1f4db0860579
Seddik <saddikgassou161718@gmail.com>
2025-05-04 22:30:51 +0100

    تعديل الدتا بيز

--- db/init.sql
+++ db/init.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
-    username VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password TEXT NOT NULL
 );
+
